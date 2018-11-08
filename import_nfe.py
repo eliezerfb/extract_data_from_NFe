102192ce4e576573f19ef2c4f0cf737cca30f020
eliezerfb <eliezerfb@gmail.com>
2018-11-08 16:44:59 -0200

    add readme

--- import_nfe.py
+++ import_nfe.py
@@ -1,21 +1,3 @@
-"""
-Importação dos dados do XML da NF-e para uma tabela em banco de dados já
-existente. Os dados importados serão utilizados para a geração de CT-e
-(Conhecimento de Frete Eletrônico) e vinculados a um Manifesto de
-Documento Fiscal Eletrônico (MDF-e).
-
-Para configurar o banco de dados é necessário criar um arquivo .env (veja em
-.env_example), com a string de conexão com o banco de dados.
-
-Para instalar as bibliotecas utilizadas neste projeto:
-pip install -r requeriments.txt
-
-Para executar é necessário passar como parâmetro o ID do manifesto. Exemplo:
-
-python import_nfe.py 317
-
-"""
-
 import glob
 import os
 import sys
